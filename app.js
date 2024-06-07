d1b05e16c3b249802b00d446f26c6456a80ee4cf
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2024-06-07 23:53:57 +0530

    Fix returning same quote one after another

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-let btn = document.querySelector('#new-quote');
-let quote = document.querySelector('.quote');
-let person = document.querySelector('.person');
-
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+let lastQuote = "";
 
 //Function to get a new quote from the API
 async function getQuote() {
     try {
-        const response = await fetch('https://api.quotable.io/random');
+        const response = await fetch("https://api.quotable.io/random");
         const data = await response.json();
-        quote.innerText = `"${data.content}"`;
+
+        //Check if the quote is the same as the last one
+        if(data.content === lastQuote){
+            return getQuote();
+            
+        }
+
+        lastQuote = data.content;
+        quote.innerText = `“${data.content}”`;
         person.innerText = `-${data.author}`;
+        
+        
 
     } catch (error) {
         console.log("Error fetching the quote",error);
@@ -19,7 +29,7 @@ async function getQuote() {
 }
 
 //Event listener to get a new quote when the button is clicked
-btn.addEventListener('click', getQuote);
+btn.addEventListener("click", getQuote);
 
 //Initial call to get a quote when the page loads
 getQuote();
